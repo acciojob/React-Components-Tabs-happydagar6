1b1eb552fa42ec2a5584cf1294d62041916f8047
Your Name <harshitdagar913@gmail.com>
2026-05-27 20:41:24 +0530

    Add basic tab structure and viewport to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
+import React from 'react';
 
+const App = () => {
+  return (
+    <div>
+      {/* Global component with className 'tabs' */}
+      <div className="tabs">
+        {/* Each tab with className 'tab' and specific id */}
+        <div className="tab" id="Home-tab">
+          Home
+        </div>
+        <div className="tab" id="About-tab">
+          About
+        </div>
+        <div className="tab" id="Features-tab">
+          Features
+        </div>
+      </div>
+
+      {/* Viewport with className 'viewport' and specific text */}
+      <div className="viewport">
+        Pages Go Here
+      </div>
+    </div>
+  );
+};
+
+export default App;
